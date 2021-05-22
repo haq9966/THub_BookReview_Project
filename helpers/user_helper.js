4c7e62aed4169b52e0c840162fa084009722373e
Muhammed Munees <muneesmmm@gmail.com>
2021-05-22 11:10:04 +0530

    search data through dropbox added

--- helpers/user_helper.js
+++ helpers/user_helper.js
@@ -1,78 +1,109 @@
-var db = require('../config/connection')
-var collection = require('../config/collection')
-const { resolve, reject } = require('promise')
-const { response } = require('express')
-const bcrypt = require('bcrypt')
-var objectId = require('mongodb').ObjectID;
+var db = require("../config/connection");
+var collection = require("../config/collection");
+const { resolve, reject } = require("promise");
+const { response } = require("express");
+const bcrypt = require("bcrypt");
+var objectId = require("mongodb").ObjectID;
 module.exports = {
-    doSignUp: (userData,callback) => {
-        return new Promise(async (resolve, reject) => {
-            userData.password = await bcrypt.hash(userData.password, 10)
-            db.get().collection(collection.USERS_COLLECTION).insertOne(userData).then((data) => {
-                resolve(data.ops[0])
-            })
-
-
-        })
-    },
-    addproducts:(products,callback)=>{
-        db.get().collection('books').insertOne(products).then((data)=>{
-
-            callback(data.ops[0]._id)
-
-        })
-    },
-    addreview:(review)=>{
-        return new Promise(async (resolve, reject) => {
-         let data=await db.get().collection(collection.REVIEW_COLLECTION).insertOne(review)
-             console.log(",,,,,,,,,,,,,,,,,,,,",data.ops[0]);
-            resolve(data.ops[0])       
-        
-    })
-    },
-doLogin  : (userData) => {
+  doSignUp: (userData, callback) => {
     return new Promise(async (resolve, reject) => {
-        let loginStatus = false
-        let respons = {}
-        let user = await db.get().collection(collection.USERS_COLLECTION).findOne({ emailid: userData.emailid })
-        if (user) {
-            bcrypt.compare(userData.password, user.password).then((status) => {
-                if (status) {
-                    console.log("success");
-                    response.user = user
-                    response.status = true
-                    resolve(response)
-                } else {
-                    console.log("failed");
-                    resolve({ status: false })
-                }
-            })
-        } else {
-            console.log("db failed");
-            resolve({ status: false })
-        }
-    })
-},
-getAllProducts:()=>{
-    return new Promise(async(resolve,reject)=>{
-        let products=await db.get().collection('books').find().toArray()
-        resolve(products)
-    })
-
-},
-getAllReviews:(id)=>{
-    return new Promise(async(resolve,reject)=>{
-        let review=await db.get().collection('review').find({bookid:id}).toArray()
-        resolve(review)
-    })
-
-},
-getSelectedProducts:(id)=>{
-    return new Promise((resolve, reject) => { 
-        console.log("..............ID :"+id+"................."); 
-        db.get().collection(collection.BOOK_COLLECTION).findOne({ _id: objectId(id)}).then((data) => {
-            resolve(data)
-        })
-        })
-}
-}
+      userData.password = await bcrypt.hash(userData.password, 10);
+      db.get()
+        .collection(collection.USERS_COLLECTION)
+        .insertOne(userData)
+        .then((data) => {
+          resolve(data.ops[0]);
+        });
+    });
+  },
+  addproducts: (products, callback) => {
+    // product.language=product.language.toLowerCase();
+    db.get()
+      .collection("books")
+      .insertOne(products)
+      .then((data) => {
+        callback(data.ops[0]._id);
+      });
+  },
+  addreview: (review) => {
+    return new Promise(async (resolve, reject) => {
+      let data = await db
+        .get()
+        .collection(collection.REVIEW_COLLECTION)
+        .insertOne(review);
+      console.log(",,,,,,,,,,,,,,,,,,,,", data.ops[0]);
+      resolve(data.ops[0]);
+    });
+  },
+  doLogin: (userData) => {
+    return new Promise(async (resolve, reject) => {
+      let loginStatus = false;
+      let respons = {};
+      let user = await db
+        .get()
+        .collection(collection.USERS_COLLECTION)
+        .findOne({ emailid: userData.emailid });
+      if (user) {
+        bcrypt.compare(userData.password, user.password).then((status) => {
+          if (status) {
+            console.log("success");
+            response.user = user;
+            response.status = true;
+            resolve(response);
+          } else {
+            console.log("failed");
+            resolve({ status: false });
+          }
+        });
+      } else {
+        console.log("db failed");
+        resolve({ status: false });
+      }
+    });
+  },
+  getAllProducts: () => {
+    return new Promise(async (resolve, reject) => {
+      let products = await db.get().collection("books").find().toArray();
+      resolve(products);
+    });
+  },
+  getProducts: (data) => {
+    return new Promise(async (resolve, reject) => {
+      let products = await db.get().collection("books").find({language:data}).toArray();
+      resolve(products);
+    });
+  },
+  getGenre: (data) => {
+    return new Promise(async (resolve, reject) => {
+      let products = await db.get().collection("books").find({Genre:data}).toArray();
+      resolve(products);
+    });
+  },
+  getbook: (data) => {
+    return new Promise(async (resolve, reject) => {
+      let products = await db.get().collection("books").find({_id:objectId(data)}).toArray();
+      resolve(products);
+    });
+  },
+  getAllReviews: (id) => {
+    return new Promise(async (resolve, reject) => {
+      let review = await db
+        .get()
+        .collection("review")
+        .find({ bookid: id })
+        .toArray();
+      resolve(review);
+    });
+  },
+  getSelectedProducts: (id) => {
+    return new Promise((resolve, reject) => {
+      console.log("..............ID :" + id + ".................");
+      db.get()
+        .collection(collection.BOOK_COLLECTION)
+        .findOne({ _id: objectId(id) })
+        .then((data) => {
+          resolve(data);
+        });
+    });
+  },
+};
--- routes/users.js
+++ routes/users.js
@@ -5,61 +5,143 @@ var express = require("express");
 var router = express.Router();
 const adminHelper = require("../helpers/admin_helper");
 const userHelper = require("../helpers/user_helper");
-const verifylogin=(req,res,next)=>{
-  if(req.session.userloggedIn){
-    next()
-  }else{
-    res.redirect('/login')
+const verifylogin = (req, res, next) => {
+  if (req.session.userloggedIn) {
+    next();
+  } else {
+    res.redirect("/login");
   }
-}
+};
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  let user=req.session.user
+  let user = req.session.user;
   userHelper.getAllProducts().then((data) => {
     console.log(response);
-    res.render("user/product",{data,user})
+    res.render("user/product", { data, user });
   });
 });
+router.get("/english", function (req, res, next) {
+  let user = req.session.user;
+  userHelper.getProducts("english").then((data) => {
+    console.log(response);
+    res.render("user/product", { data, user });
+  });
+});
+router.get("/malayalam", function (req, res, next) {
+  let user = req.session.user;
+  userHelper.getProducts("malayalam").then((data) => {
+    console.log(response);
+    res.render("user/product", { data, user });
+  });
+});
+router.get("/hindi", function (req, res, next) {
+  let user = req.session.user;
+  userHelper.getAllProducts("hindi").then((data) => {
+    console.log(response);
+    res.render("user/product", { data, user });
+  });
+});
+router.get("/fantacy", function (req, res, next) {
+  let user = req.session.user;
+  userHelper.getGenre("Fantasy").then((data) => {
+    console.log(response);
+    if(data)
+    res.render("user/product", { data, user });
+    else
+    res.render("user/empty",{user});
+
+  });
+});
+router.get("/action", function (req, res, next) {
+  let user = req.session.user;
+  userHelper.getGenre("Action").then((data) => {
+    console.log(response);
+    res.render("user/product", { data, user });
+  });
+});
+router.get("/drama", function (req, res, next) {
+  let user = req.session.user;
+  userHelper.getGenre("Drama").then((data) => {
+    console.log(response);
+    res.render("user/product", { data, user });
+  });
+});
+router.get("/comedy", function (req, res, next) {
+  let user = req.session.user;
+  userHelper.getGenre("Comedy").then((data) => {
+    res.render("user/product", { data, user });
+  });
+});
+router.get("/data/:id", function (req, res, next) {
+  let user = req.session.user;
+  userHelper.getbook(req.params.id).then((data) => {
+    res.render("user/product", { data, user });
+  });
+});
+router.get("/tragedy", function (req, res, next) {
+  let user = req.session.user;
+  userHelper.getGenre("Tragedy").then((data) => {
+    console.log(response);
+    res.render("user/product", { data, user });
+  });
+});
+router.get("/slice", function (req, res, next) {
+  let user = req.session.user;
+  userHelper.getGenre("Slice of Life").then((data) => {
+    res.render("user/product", { data, user });
+  });
+});
+// router.get("/fantacy", function (req, res, next) {
+//   let user = req.session.user;
+//   userHelper.getGenre("Fantasy").then((data) => {
+//     console.log(response);
+//     res.render("user/product", { data, user });
+//   });
+// });
 router.get("/login", function (req, res, next) {
-  if(req.session.user){
-    res.redirect('/')
-  }else{
-  res.render("user/login",{"loginErr":req.session.userloginErr});
-  req.session.userloginErr=false
+  if (req.session.user) {
+    res.redirect("/");
+  } else {
+    res.render("user/login", { loginErr: req.session.userloginErr });
+    req.session.userloginErr = false;
   }
 });
 router.post("/signup", (req, res) => {
   userHelper.doSignUp(req.body).then((response) => {
-
     let image = req.files.dp;
-    image.mv("./public/productimages/" + response.username + ".jpg", (err, done) => {
-    });
+    image.mv(
+      "./public/productimages/" + response.username + ".jpg",
+      (err, done) => {}
+    );
     console.log(response);
     res.redirect("/login");
   });
 });
 router.post("/login", (req, res) => {
   userHelper.doLogin(req.body).then((response) => {
-    if(response.status){
-      req.session.user=response.user
-      req.session.userloggedIn=true
-  
-      res.redirect('/')
-    }else{
-      req.session.userloginErr="invalid username & password"
-      res.redirect('/login')
+    if (response.status) {
+      req.session.user = response.user;
+      req.session.userloggedIn = true;
+
+      res.redirect("/");
+    } else {
+      req.session.userloginErr = "invalid username & password";
+      res.redirect("/login");
     }
   });
 });
-router.get('/details/:id',async(req,res)=>{
-  
-let product=await userHelper.getSelectedProducts(req.params.id)
-user=req.session.user
-reviews=await userHelper.getAllReviews(req.params.id)
-   console.log(reviews);
-  res.render('user/details',{product,reviews,user})
-})
-router.get("/addproducts", verifylogin,function (req, res, next) {
+router.get("/details/:id", async (req, res) => {
+  let product = await userHelper.getSelectedProducts(req.params.id);
+  user = req.session.user;
+  reviews = await userHelper.getAllReviews(req.params.id);
+  console.log(reviews);
+  res.render("user/details", { product, reviews, user });
+});
+router.get("/empty", (req, res) => {
+  user = req.session.user;
+  res.render("user/empty", { user });
+});
+router.get("/addproducts", verifylogin, function (req, res, next) {
   res.render("user/addproducts");
 });
 router.post("/addproducts", (req, res) => {
@@ -68,7 +150,7 @@ router.post("/addproducts", (req, res) => {
     console.log(id);
     image.mv("./public/productimages/" + id + ".jpg", (err, done) => {
       if (!err) {
-        res.render("user/addproducts",{user:req.session.user});
+        res.render("user/addproducts", { user: req.session.user });
       } else {
         console.log(err);
       }
@@ -76,14 +158,14 @@ router.post("/addproducts", (req, res) => {
   });
 });
 router.post("/addreview", (req, res) => {
-  userHelper.addreview(req.body).then(()=>{
-    res.redirect('/')
-    })
+  userHelper.addreview(req.body).then(() => {
+    res.redirect("/");
+  });
 });
-router.get('/logout',(req,res)=>{
-  req.session.userloggedIn=false
-  req.session.user=null
-  res.redirect('/')
-})
+router.get("/logout", (req, res) => {
+  req.session.userloggedIn = false;
+  req.session.user = null;
+  res.redirect("/");
+});
 
 module.exports = router;
